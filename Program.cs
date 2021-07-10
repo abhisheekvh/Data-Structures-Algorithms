2ea766c817be7f33ee214b736d5c15e4a41d4d62
Abhishek <hayagreevvishnu@gmail.com>
2021-07-10 22:57:09 +0530

    Encryption for given string

--- Program.cs
+++ Program.cs
@@ -1,83 +1,95 @@
-﻿
-using System;
-class MergeSort {
-
-	void merge(int[] arr, int l, int m, int r)
-	{
-	
-		int n1 = m - l + 1;
-		int n2 = r - m;
-
-
-		int[] L = new int[n1];
-		int[] R = new int[n2];
-		int i, j;
-
-
-		for (i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
-
-		i = 0;
-		j = 0;
-		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
+﻿using System;
+class Encription 
+{
+    private static Tuple<string,string> Encript(string str,int n)
+    {
+        char c;
+        int count=0;
+        int []store=new int[n];
+        int ct=0;
+		int pos=0;
+		string singleVal="";
+		int i;
+        for(i=0;i<n;i=i+pos)
+        {
+            c=str[i];
+            for(int j=i;j<n;++j)
+            {
+                if(str[j]==c)
+                {
+                    count++;
+					pos++;
+					if(!singleVal.Contains(str[i]))
+					{
+						singleVal+=str[i];
+					}
+                }
+            }
+            store[ct++]=count;
+            count=0;
+        }
+		for(int k=pos;k<n;k++)
+		{
+			count++;
+			if(!singleVal.Contains(str[k]))
+			{
+				singleVal+=str[k];
 			}
-			else {
-				arr[k] = R[j];
-				j++;
+		}
+		store[ct++]=count;
+		string hexValue = "";
+		string Encripted="";
+		System.Console.WriteLine(hexValue);
+		System.Console.WriteLine();
+        for(int z=0;z<ct;z++)
+       	{
+			if(store[z]>0)
+            	hexValue += store[z].ToString("X");
+    	}
+		string enc=singleVal+hexValue;
+		for(int l=enc.Length-1;l>=0;l--)
+			 	Encripted+=enc[l];
+		int []digit=new int[enc.Length];
+		int digitCount=0;
+		string []stringItem=new string[enc.Length];
+		int stringCount=0;
+		for(int m=0;m<Encripted.Length;m++)
+		{
+			if(Encripted[m]>'0' && Encripted[m]<='9')
+			{
+				digit[digitCount++]=Encripted[m];
 			}
-			k++;
+			else
+			{
+				stringItem[stringCount++]=Encripted[m].ToString();
+			}
 		}
-
-		while (i < n1) {
-			arr[k] = L[i];
-			i++;
-			k++;
+		int st=0,end=0;
+		string finalEncripted="";
+		object []final=new object[enc.Length];
+		int finCount=0;
+		while(st<digitCount && end<stringCount)
+		{
+			final[finCount++]=singleVal[st++];
+			final[finCount++]=store[end++];
 		}
-
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
+		if(final!=null)
+		{
+			for(int l=final.Length-1;l>=0;l--)
+			finalEncripted+=final[l];
 		}
-	}
+		return Tuple.Create(Encripted,finalEncripted);
+    }
+    public static void Main(String[] args)
+    {
+        string str="aaabbbcc";
+        int len=str.Length;
+        var encriptedVal=Encript(str,len);
+		if(encriptedVal.Item1!=null &&encriptedVal.Item1!="")
+		System.Console.WriteLine("Encrypted value: "+encriptedVal.Item1);
+		if(encriptedVal.Item2!=null && encriptedVal.Item2!="")
+		System.Console.WriteLine("Second Encrypted Value: "+encriptedVal.Item2);
+		System.Console.WriteLine();
+    }
 
-	void sort(int[] arr, int l, int r)
-	{
-		if (l < r) {
-		
-
-			int m = l+ (r-l)/2;
-
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
-
-			merge(arr, l, m, r);
-		}
-	}
-
-	static void printArray(int[] arr)
-	{
-		int n = arr.Length;
-		for (int i = 0; i < n; ++i)
-			Console.Write(arr[i] + " ");
-		Console.WriteLine();
-	}
-
-
-	public static void Main(String[] args)
-	{
-		int[] arr = { 12, 11, 13, 5, 6, 7 };
-		Console.WriteLine("Given Array");
-		printArray(arr);
-		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.Length - 1);
-		Console.WriteLine("\nSorted array");
-		printArray(arr);
-	}
 }
