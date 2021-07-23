33c74c1fdec55a13e53c904df4e7d8086b72edb9
Abhishek <hayagreevvishnu@gmail.com>
2021-07-23 22:26:02 +0530

    Number of zero's between one's

---
+++ NumberOFZeros.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onezero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 0;
+            Console.WriteLine("Enter string Values containing 1's and 0's");
+            string str = Console.ReadLine();
+            Console.WriteLine();
+            string s = "";
+            int end = str.Length - 1;
+            while (str[end] != '1')
+                end--;
+            for (int i = 0; i <= end; i++)
+                s += str[i];
+            for (int i = 0; i <= s.Length; i++)
+            {
+                if (i == s.Length - 1)
+                    break;
+                if (s[i] == '1')
+                    while (s[i + 1] != '1')
+                    {
+                        count++;
+                        i++;
+                    }
+            }
+            Console.WriteLine("Number of 0's between 1's are " + count);
+            Console.Read();
+        }
+    }
+}
