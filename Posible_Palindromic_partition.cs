6fb2136c2f86c7bcc3788b84600e1f3e1b1e3fe2
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-04 10:39:15 +0530

    Posible_Palindromic_partition

---
+++ Posible_Palindromic_partition.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class PrintAllPalindrome
+{
+
+	public static void Main(String[] args)
+	{
+		print(3);
+		Console.Read();
+	}
+	private static void print(int n)
+    {
+		if(n>0)
+        {
+			print(n - 1);
+			Console.Write(n+ " ");
+			
+        }
+    }
+
+	
+	
+}
+
