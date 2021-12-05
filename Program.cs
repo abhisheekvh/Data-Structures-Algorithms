3c2882ef9cbd8d96dca1f9a3d26ac8defcea39c0
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-05 21:58:08 +0530

    longest_prefix

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace recursionPractice
+{
+    class Program
+    {
+       private static string common(string []str)
+        {
+            int size = str.Length;
+            if (size == 0)
+                return " ";
+            if (size == 1)
+                return str[0];
+             Array.Sort(str);
+            int end = Math.Min(str[0].Length, str[size - 1].Length);
+            int i = 0;
+            while (i < end && str[0][i] == str[size - 1][i])
+                i++;
+            string prev = str[0].Substring(0, i);
+            return prev;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            string[] arr = { "glower","gmow","glo" };
+            string res = common(arr);
+            Console.WriteLine(res);
+            
+            Console.Read();
+
+        }
+    }
+}
