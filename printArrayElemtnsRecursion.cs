795243d6a1b558dc113eb8632ce59b39aeab5ac0
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-08 21:43:42 +0530

    print array elements using recursion

---
+++ printArrayElemtnsRecursion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace recursionPractice
+{
+    class Program
+    {
+        public static int i = 0;
+        private static void sum(int []arr,int size)
+        {
+
+            if(arr.Length-1==size)
+                Console.Write(arr[arr.Length-1]+" ");  //base condition
+            else
+            {
+                Console.Write(arr[i++]+ " ");
+                sum(arr, i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 4, 5, 6 };
+            sum(arr,0);
+            Console.Read();
+
+        }
+    }
+}
