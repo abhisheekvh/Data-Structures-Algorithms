25730d28d964eb4d92f3e229992a89156a6b45df
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-09 19:13:40 +0530

    Power of given number with reduced ime complexity

---
+++ poerOfnumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace recursion
+{
+    class Program
+    {
+        private static int power(int m,int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                if(n%2==0)
+                    return power(m * m, n / 2);
+                else
+                    return m * power(m*m, (n - 1) / 2);
+        }
+        static void Main(string[] args)
+        {
+            int res = power(2, 9);
+            Console.WriteLine(res);
+            Console.Read();
+        }
+    }
+}
